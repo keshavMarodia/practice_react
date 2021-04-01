5c63cd072dd181948695bb137dc402c7ffd346cd
keshavMarodia <72756724+keshavMarodia@users.noreply.github.com>
2021-04-01 17:54:42 +0530

    Update india.jsx

--- src/india.jsx
+++ src/india.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import "./styles.css";
 
 export function Keshav() {
   return <h1>hello world i am keshav marodia</h1>;
+}
+export function printonce() {
+  return <h3 className="printonce">hello world </h3>;
+}
+export function printtwice() {
+  return (
+    <div className="printtwice">
+      <span>hello world</span>
+      <span>hello world</span>
+    </div>
+  );
 }
 
 export default Keshav;
